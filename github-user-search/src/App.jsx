d45d0f20ecf4fc28c5b01398e2f6ef6c5512429c
Nwokocha Amarachi <114367454+AMARAXO@users.noreply.github.com>
2024-09-17 06:10:13 +0100

    profile find

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Search from './components/Search';
 
 const App = () => {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <input type="text" placeholder="Search for a GitHub user..." />
-      {/* Results will be displayed here */}
+      <Search />
     </div>
+  
   );
 };
 
